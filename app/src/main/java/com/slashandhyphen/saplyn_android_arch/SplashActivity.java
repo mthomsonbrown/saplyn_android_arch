e160a35fffd588e446a7dc0cf922eb6288285937
Mike <mthomsonbrown@gmail.com>
2017-12-26 19:54:48 -0800

    We have a button!

--- app/src/main/java/com/slashandhyphen/saplyn_android_arch/SplashActivity.java
+++ app/src/main/java/com/slashandhyphen/saplyn_android_arch/SplashActivity.java
@@ -1,31 +1,27 @@
 package com.slashandhyphen.saplyn_android_arch;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class SplashActivity extends AppCompatActivity {
+
+public class SplashActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        button_home = findViewById(R.id.button_home);
+        button_home.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +44,10 @@ public class SplashActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Snackbar.make(v, "Hello", Snackbar.LENGTH_INDEFINITE).show();
     }
 }
