6e8e0d1d4a8d665b655c45da75b18695f947f29a
Mike <mthomsonbrown@gmail.com>
2018-01-15 21:16:52 -0800

    Added a clik button in the entries fragment

--- app/src/main/java/com/slashandhyphen/saplyn_android_arch/view/entry/EntryActivityFragment.java
+++ app/src/main/java/com/slashandhyphen/saplyn_android_arch/view/entry/EntryActivityFragment.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.slashandhyphen.saplyn_android_arch.R;
 import com.slashandhyphen.saplyn_android_arch.model.Database;
@@ -51,6 +53,8 @@ public class EntryActivityFragment extends Fragment implements View.OnClickListe
                 R.layout.fragment_entry, container, false);
         recycler = layout.findViewById(R.id.recycler_view_entries);
         clickList = new ArrayList<>();
+        Button clickButton = layout.findViewById(R.id.button_click);
+        clickButton.setOnClickListener(this);
 
         // Prepare RecyclerView
         adapter = new EntryAdapter(clickList);
@@ -72,6 +76,8 @@ public class EntryActivityFragment extends Fragment implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        if(v.getId() == R.id.button_click) {
+            Toast.makeText(this.getActivity(), "Clicked the button", Toast.LENGTH_LONG).show();
+        }
     }
 }
